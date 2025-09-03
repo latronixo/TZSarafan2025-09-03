8f73dfce508e61e043f61d5cb7c6041f1e910ea9
Valentin <pamerra@mail.ru>
2025-09-03 15:40:53 +0300

    Initial Commit

---
+++ TZSarafan2025-09-03/TZSarafan2025_09_03App.swift
@@ -0,0 +1,17 @@
+//
+//  TZSarafan2025_09_03App.swift
+//  TZSarafan2025-09-03
+//
+//  Created by Валентин on 03.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct TZSarafan2025_09_03App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
